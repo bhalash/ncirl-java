c547dd92b9b28d467d7d03e86f69bd64eba0670b
Mark Grealish <mark@bhalash.com>
2025-09-29 16:43:17 +0100

    add week one exercises

---
+++ src/main/java/com/bhalash/ncirl/weekOne/Diamond.java
@@ -0,0 +1,13 @@
+package com.bhalash.ncirl.weekOne;
+
+public class Diamond {
+    public static void main(String[] args) {
+        System.out.println("""
+                  *
+                 * *
+                * * *
+                 * *
+                  *
+                """);
+    }
+}
---
+++ src/main/java/com/bhalash/ncirl/weekOne/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package com.bhalash.ncirl.weekOne;
+
+//class definition
+public class MyFirstProgram {
+  // main method header
+  public static void main(String[] args) {
+    System.out.println("I love Maths");
+    System.out.println("And I love java too");
+  }
+}
